a17ad23a9593156b0e233880e80d8c83d56cea02
Filip Krasniqi <fkrasniqi@beckman.com>
2024-01-23 20:42:50 +0000

    feat(utils): added convert_to_corners and get_dataset_path

--- model/utils/utils.py
+++ model/utils/utils.py
@@ -1,41 +1,22 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+import pandas as pd
 import tensorflow as tf
+from dotenv import load_dotenv
+from functools import partial
 from tensorflow import keras
+from tqdm import tqdm
+from typing import Tuple
+
+from model.utils.inference import resize_and_pad_image
+
+load_dotenv()
 
 
-def swap_xy(boxes):
-    """Swaps order the of x and y coordinates of the boxes.
-
-    Arguments:
-      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
-
-    Returns:
-      swapped boxes with shape same as that of boxes.
-    """
-    return tf.stack([boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]],
-                    axis=-1)
+def get_dataset_path() -> str:
+    return os.environ["DATASET_PATH"]
 
 
-def convert_to_xywh(boxes):
-    """Changes the box format to center, width and height.
-
-    Arguments:
-      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
-        representing bounding boxes where each box is of the format
-        `[xmin, ymin, xmax, ymax]`.
-
-    Returns:
-      converted boxes with shape same as that of boxes.
-    """
-    return tf.concat(
-        [(boxes[..., :2] + boxes[..., 2:]) / 2.0,
-         boxes[..., 2:] - boxes[..., :2]],
-        axis=-1,
-    )
-
-
-def convert_to_corners(boxes):
+def convert_to_corners(boxes: tf.Tensor) -> tf.Tensor:
     """Changes the box format to corner coordinates
 
     Arguments:
@@ -54,8 +35,43 @@ def convert_to_corners(boxes):
         axis=-1,
     )
 
+def swap_xy(boxes):
+    """Swaps order the of x and y coordinates of the boxes.
 
-def compute_iou(boxes1, boxes2):
+    Arguments:
+      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+
+    Returns:
+      swapped boxes with shape same as that of boxes.
+    """
+    return tf.stack(
+        [boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]],
+        axis=-1,
+    )
+
+
+def convert_to_xywh(boxes: tf.Tensor) -> tf.Tensor:
+    """Changes the box format to center, width and height.
+
+    Arguments:
+      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[xmin, ymin, xmax, ymax]`.
+
+    Returns:
+      converted boxes with shape same as that of boxes.
+    """
+    return tf.concat(
+        [(boxes[..., :2] + boxes[..., 2:]) / 2.0,
+         boxes[..., 2:] - boxes[..., :2]],
+        axis=-1,
+    )
+
+
+def compute_iou(
+    boxes1: tf.Tensor,
+    boxes2: tf.Tensor,
+) -> tf.Tensor:
     """Computes pairwise IOU matrix for given two sets of boxes
 
     Arguments:
@@ -82,46 +98,10 @@ def compute_iou(boxes1, boxes2):
     return tf.clip_by_value(intersection_area / union_area, 0.0, 1.0)
 
 
-def visualize_detections(image,
-                         boxes,
-                         classes,
-                         scores,
-                         figsize=(7, 7),
-                         linewidth=1,
-                         color=[0, 0, 1]):
-    """Visualize Detections"""
-    image = np.array(image, dtype=np.uint8)
-    plt.figure(figsize=figsize)
-    plt.axis("off")
-    plt.imshow(image)
-    ax = plt.gca()
-    for box, _cls, score in zip(boxes, classes, scores):
-        text = "{}: {:.2f}".format(_cls, score)
-        x1, y1, x2, y2 = box
-        w, h = x2 - x1, y2 - y1
-        patch = plt.Rectangle([x1, y1],
-                              w,
-                              h,
-                              fill=False,
-                              edgecolor=color,
-                              linewidth=linewidth)
-        ax.add_patch(patch)
-        ax.text(
-            x1,
-            y1,
-            text,
-            bbox={
-                "facecolor": color,
-                "alpha": 0.4
-            },
-            clip_box=ax.clipbox,
-            clip_on=True,
-        )
-    plt.show()
-    return ax
-
-
-def random_flip_horizontal(image, boxes):
+def random_flip_horizontal(
+    image: tf.Tensor,
+    boxes: tf.Tensor,
+) -> Tuple[tf.Tensor, tf.Tensor]:
     """Flips image and boxes horizontally with 50% chance
 
     Arguments:
@@ -141,54 +121,11 @@ def random_flip_horizontal(image, boxes):
     return image, boxes
 
 
-def resize_and_pad_image(image,
-                         min_side=800.0,
-                         max_side=1333.0,
-                         jitter=[640, 1024],
-                         stride=128.0):
-    """Resizes and pads image while preserving aspect ratio.
-
-    1. Resizes images so that the shorter side is equal to `min_side`
-    2. If the longer side is greater than `max_side`, then resize the image
-      with longer side equal to `max_side`
-    3. Pad with zeros on right and bottom to make the image shape divisible by
-    `stride`
-
-    Arguments:
-      image: A 3-D tensor of shape `(height, width, channels)` representing an
-        image.
-      min_side: The shorter side of the image is resized to this value, if
-        `jitter` is set to None.
-      max_side: If the longer side of the image exceeds this value after
-        resizing, the image is resized such that the longer side now equals to
-        this value.
-      jitter: A list of floats containing minimum and maximum size for scale
-        jittering. If available, the shorter side of the image will be
-        resized to a random value in this range.
-      stride: The stride of the smallest feature map in the feature pyramid.
-        Can be calculated using `image_size / feature_map_size`.
-
-    Returns:
-      image: Resized and padded image.
-      image_shape: Shape of the image before padding.
-      ratio: The scaling factor used to resize the image
-    """
-    image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
-    if jitter is not None:
-        min_side = tf.random.uniform((), jitter[0], jitter[1], dtype=tf.float32)
-    ratio = min_side / tf.reduce_min(image_shape)
-    if ratio * tf.reduce_max(image_shape) > max_side:
-        ratio = max_side / tf.reduce_max(image_shape)
-    image_shape = ratio * image_shape
-    image = tf.image.resize(image, tf.cast(image_shape, dtype=tf.int32))
-    padded_image_shape = tf.cast(tf.math.ceil(image_shape / stride) * stride,
-                                 dtype=tf.int32)
-    image = tf.image.pad_to_bounding_box(image, 0, 0, padded_image_shape[0],
-                                         padded_image_shape[1])
-    return image, image_shape, ratio
-
-
-def preprocess_data(sample):
+def preprocess_data_tuple(
+    sample: Tuple,
+    swap: bool = False,
+    multiply_bbox: bool = False,
+) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
     """Applies preprocessing step to a single sample
 
     Arguments:
